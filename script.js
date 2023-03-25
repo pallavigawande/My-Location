971ae5e6466485083560260461412415d04595ba
pallavigawande <95491010+pallavigawande@users.noreply.github.com>
2023-03-25 23:45:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(showPosition);
+  btn.disabled = true;
+} else {
+  map.innerText = "Sorry to say Geolocation is not supported by your browser";
+}
+function showPosition(position) {
+  localStorage.setItem("lat", position.coords.latitude);
+  localStorage.setItem("long", position.coords.longitude);
+  map.innerHTML = `<iframe src="https://maps.google.com/maps?q=${position.coords.latitude}, ${position.coords.longitude}&output=embed"></iframe>`;
+}
+function removeLocation() {
+  btn.disabled = false;
+  localStorage.removeItem("lat");
+  localStorage.removeItem("long");
+}
